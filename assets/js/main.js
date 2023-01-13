cd94d6bd7e94df35a04c7e46bcba0884238a70d2
Stefan Endres <info@stefanendres.com>
2023-01-13 12:00:10 +0100

    add first javascript workflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
 window.onload = () => {
-    console.log(lazySizes)
-    console.log('hellohello')
-}
+    console.log('hellohello');
+    //console.log(window);
+    //console.log(window.navigator);
+    //console.log(document);
+
+
+    let main = document.querySelector('main');
+    main.classList.add('loaded');
+
+
+    let headerLinks = document.querySelector('header').querySelectorAll('.menu-link');
+
+    let animatedElement = document.querySelector('.site-logo')
+
+    headerLinks.forEach((headerLink) => {
+        headerLink.addEventListener('click', (event) => {
+            main.classList.remove('loaded');
+            animatedElement.style.animationDuration = 1+'s';
+        });
+    });
+
+
+    let projectLinks = document.querySelectorAll('.project-link');
+
+    projectLinks.forEach((projectLink) => {
+        projectLink.addEventListener('click', (event) => {
+            main.classList.remove('loaded');
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+        console.log(window.scrollY)
+    })
+
+};
